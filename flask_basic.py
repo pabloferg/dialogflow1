9e1cfba3769eb74031f5066482703b6da1639f4e
pabloferg <51671788+pabloferg@users.noreply.github.com>
2019-06-12 18:06:58 +0100

    Add files via upload

---
+++ flask_basic.py
@@ -0,0 +1,140 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+import requests
+import time
+from google.cloud import bigquery
+
+# Initialize a BigQuery Client
+client = bigquery.Client()
+
+# Flask application
+application = Flask(__name__)
+
+@application.route("/", methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+        some_json = request.get_json()
+        return jsonify({'you sent' : some_json}), 201
+    else:
+        return jsonify({"about" : "Hello World"})   # we are only GETting info from the API so far
+
+# <destination> is the parameter used in Dialogflow to call the url, i.e. digital-flights.herokuapp.com/destination/madrid
+@application.route('/destination/<destination>', methods=['GET'])
+def get_destination(destination):
+    """ 1. Get Airport Code and Airport Name using lookup table in BigQuery
+        2. Using Airport Code, call Amadeus API to retrieve fares
+
+
+
+    """
+    # converts cityName (i.e 'Valencia') to airport Code and Name (i.e 'VLC', 'Valencia Airport')
+    airport_code, airport_name = cityName_to_airport(cityName = destination) # If not match, variables = empty
+
+    # this is just a joke
+    if destination.lower() not in ['hogwarts', 'mordor']:
+
+        # if destination not in BigQuery Table
+        if airport_code == "":
+            json_output = jsonify({"error" : "Sorry. We don't fly to %s" % (destination)})
+        else:
+            # if airport code detected, call Amadeus API
+            json = get_request_fares(access_token = post_request_fares(), airport_code=airport_code)
+            # try, if except (route from Heathrow does not exist) return full error message in json response
+            try:
+                # Call Amadeus API
+                fare = extract_fare_from_json(json)[0]
+                airline = extract_fare_from_json(json)[1]
+
+                json_output = jsonify({"fare" : fare,
+                                "airline" : airline,
+                                "airportCode" : airport_code,
+                                "airportName" : airport_name})
+            except:
+                json_output = jsonify(json)
+
+    else:
+        json_output = jsonify({"fare" : '300',
+                        "airline" : 'BroomAir',
+                        "airportCode" : airport_code,
+                        "airportName" : airport_name})
+
+    return json_output
+
+
+
+# AMADEUS API
+def post_request_fares():
+
+    url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+
+    payload = "grant_type=client_credentials&client_id=gHea0Lv9FKuZmoDAsKblP4KmU3YcLGu6&client_secret=M6cweU8QmCb7XhNR&undefined="
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'cache-control': "no-cache",
+        'Postman-Token': "52a00ceb-c42c-46bc-a9b7-3afb5f693441"
+        }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    return response.json()['access_token']
+
+def get_request_fares(access_token, airport_code):
+
+    url = "https://test.api.amadeus.com/v1/shopping/flight-offers"
+
+    querystring = {"origin":"LHR",
+                   "destination": airport_code,
+                   "departureDate":"2019-07-01",
+                   "adults":"1",
+                   "includeAirlines":"BA",
+                   "nonStop":"false",
+                   "max":"1"}
+
+    payload = ""
+    headers = {
+        'Authorization': "Bearer " + access_token,
+        'cache-control': "no-cache",
+        }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    return response.json()
+
+def extract_fare_from_json(json):
+    fare = json['data'][0]['offerItems'][0]['pricePerAdult']['total']
+    airline = json['data'][0]['offerItems'][0]['services'][0]['segments'][0]['flightSegment']['carrierCode']
+
+    return fare, airline
+
+
+def cityName_to_airport(cityName):
+    """
+    Converts name of the city to the airport code using BigQuery
+
+    cityName : string
+    """
+
+
+    # create query string
+    query_string = """
+        SELECT * FROM AirportCityCodes.main
+        WHERE LOWER(cityName) = '%s'
+        """ % cityName.lower()
+
+    # query
+    query_job = client.query(query_string)
+
+    results = query_job.result()  # Waits for job to complete.
+    df = results.to_dataframe()   # Converts result to dataframe to manipulate it easier (note: check how to do it straightaway)
+
+    if df.shape[0] == 0:         # df is empty   (0 rows)
+        airportCode = ""
+        airportName = ""
+    elif df.shape[0] == 1:       # unique result (1 rows)
+        airportCode = df['airportCode'][0]     # pick the first airport
+        airportName = df['airportName'][0]
+    else:
+        airportCode = df['airportCode'][0]     # pick the first airport
+        airportName = df['airportName'][0]
+
+    return airportCode, airportName
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from flask_basic import application
+
+if __name__ == "__main__":
+    application.run()
